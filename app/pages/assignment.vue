3f1a62559f08d069ebfe2da9322a183f69964192
Tom <tom.van.veen@visma.com>
2025-03-06 09:56:36 +0100

    chore: upgrade packages to latest versions

--- app/pages/assignment.vue
+++ app/pages/assignment.vue
@@ -1,5 +1,12 @@
+<script lang="ts" setup>
+const route = useRoute()
+const { data: page } = await useAsyncData(route.path, () => {
+  return queryCollection('content').path(route.path).first()
+})
+</script>
+
 <template>
   <UContainer :ui="{ constrained: 'max-w-3xl' }">
-    <ContentDoc class="prose lg:prose-xl !max-w-none dark:prose-invert my-4 lg:my-8 prose-a:prose-headings:no-underline prose-headings:font-serif prose-headings:text-stone-800 dark:prose-headings:text-stone-200 prose-a:no-underline hover:prose-a:underline" />
+    <ContentRenderer v-if="page" :value="page" class="prose lg:prose-xl !max-w-none dark:prose-invert my-4 lg:my-8 prose-a:prose-headings:no-underline prose-headings:font-serif prose-headings:text-stone-800 dark:prose-headings:text-stone-200 prose-a:no-underline hover:prose-a:underline" />
   </UContainer>
 </template>
