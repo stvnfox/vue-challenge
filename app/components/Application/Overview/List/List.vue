2519042f6b939d5af93a4f3c30649ba9728fc74a
steven.vos <steven.vos@iodigital.com>
2025-03-14 19:59:14 +0100

    feat(list overview): add table component for list view

--- app/components/Application/Overview/List/List.vue
+++ app/components/Application/Overview/List/List.vue
@@ -1,11 +1,65 @@
 <script setup lang="ts">
+import type { TableColumn } from '@nuxt/ui'
+import { h, resolveComponent } from 'vue'
+
 const type = inject('type')
 
 const { items } = useUniverse(type)
+
+const UAvatar = resolveComponent('UAvatar')
+const UButton = resolveComponent('UButton')
+
+interface OverviewItem {
+  image: string
+  name: string
+  url: string
+}
+
+const columns: TableColumn<OverviewItem>[] = [
+  {
+    accessorKey: 'image',
+    header: '',
+    cell: ({ row }) => {
+      return h(UAvatar, {
+        src: row.getValue('image'),
+        alt: row.getValue('name'),
+        size: '3xl',
+        class: 'rounded-full',
+        ui: {
+          root: 'bg-transparent size-16',
+        },
+      })
+    },
+  },
+  {
+    accessorKey: 'name',
+    header: '',
+    cell: ({ row }) => {
+      return h('div', { class: 'text-lg font-bold capitalize' }, row.getValue('name'))
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      return h(
+        'div',
+        { class: 'text-right' },
+        h(UButton, {
+          icon: 'i-lucide-arrow-right',
+          class: 'ml-auto',
+          to: row.getValue('url'),
+        }, () => 'See details'),
+      )
+    },
+  },
+]
 </script>
 
 <template>
-  <div class="">
-    <pre>{{ items }}</pre>
-  </div>
+  <UTable
+    :data="items"
+    :columns="columns"
+    class="flex-1"
+    :ui="{ thead: 'hidden', tbody: 'border-y border-neutral-800', td: 'first:w-4' }"
+  />
 </template>
