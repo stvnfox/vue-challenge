f1b5095d6b10e2f37cf7a80f7a867541ea784a12
steven.vos <steven.vos@iodigital.com>
2025-03-14 18:59:49 +0100

    feat(composable): add universe composable w/ pokemon type

---
+++ app/composables/useUniverse.ts
@@ -0,0 +1,34 @@
+type UniverseKey = keyof typeof universes
+
+interface PokemonApiResponse {
+  results: { name: string, url: string }[]
+}
+
+function extractPokemonId(url: string): string {
+  const matches = url.match(/\/pokemon\/(\d+)\//)
+  return matches && matches[1] ? matches[1] : '1'
+}
+
+const universes = {
+  pokemon: {
+    fetchData: () => usePokemonData('pokemon'),
+    formatItems: (items: PokemonApiResponse) => items.results.map(item => ({
+      name: item.name,
+      id: crypto.randomUUID(),
+      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${extractPokemonId(item.url)}.png`,
+    })) || [],
+  },
+}
+
+export function useUniverse(universe: UniverseKey) {
+  const universeHandler = universes[universe]
+  const { data, status } = universeHandler.fetchData()
+
+  const formattedItems = computed(() => universeHandler.formatItems(data.value as PokemonApiResponse))
+
+  return {
+    items: formattedItems,
+    status,
+    universe,
+  }
+}
