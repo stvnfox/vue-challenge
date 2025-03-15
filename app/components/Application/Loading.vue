59b9543809cba44a026393dd98f6dba373029ffc
steven.vos <steven.vos@iodigital.com>
2025-03-15 19:32:15 +0100

    feat(loading): add loading component

---
+++ app/components/Application/Loading.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center justify-center my-12 md:my-24">
+    <UIcon name="i-lucide-loader-circle" class="animate-spin text-(--ui-primary) size-12" />
+  </div>
+</template>
