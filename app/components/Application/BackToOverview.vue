ebcb9badd60e73376bdd5ffb291d386a6160bc79
steven.vos <steven.vos@iodigital.com>
2025-03-15 19:32:31 +0100

    feat(back-to-overview): add a back-to-overview component

---
+++ app/components/Application/BackToOverview.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  to: string
+}>()
+</script>
+
+<template>
+  <UButton :to="props.to" variant="link" icon="i-lucide-arrow-left">
+    Back to overview
+  </UButton>
+</template>
