26de375e232067a497b7cf07caea81120782d907
steven.vos <steven.vos@iodigital.com>
2025-03-15 08:47:33 +0100

    feat(navigation): add active state to navigation item - also on detail pages

--- app/components/Application/Header/Link.vue
+++ app/components/Application/Header/Link.vue
@@ -3,8 +3,24 @@ const props = defineProps<{
   to: string
   label: string
 }>()
+
+const route = useRoute()
+const isActive = computed(() => {
+  if (props.to === '/') {
+    return route.path === '/'
+  }
+
+  return route.path.startsWith(props.to)
+})
 </script>
 
 <template>
-  <UButton :to="props.to" :label="props.label" variant="ghost" />
+  <!-- Added active state by adding a class to the button bc :active doesn't work - maybe because of mismatch docs version and version used in project -->
+  <UButton
+    :to="props.to"
+    :label="props.label"
+    :class="{ 'bg-blue-100 dark:bg-neutral-900': isActive }"
+    :active="isActive"
+    variant="ghost"
+  />
 </template>
