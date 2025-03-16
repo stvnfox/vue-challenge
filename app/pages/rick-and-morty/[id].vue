4490f9f72889ddfbe0393048879e571b0d2616bd
steven.vos <steven.vos@iodigital.com>
2025-03-16 10:05:04 +0100

    feat(detail page): create detail page w/ some data for rick-and-morty

--- app/pages/rick-and-morty/[id].vue
+++ app/pages/rick-and-morty/[id].vue
@@ -1,16 +1,52 @@
 <script setup lang="ts">
+import type { RickAndMortyCharacter } from '~/types/characters'
+
 const { id } = useRoute('rick-and-morty-id').params
-const { data, status } = useUniverseDetail('rick-and-morty', id)
+const { data, status } = useUniverseDetail<RickAndMortyCharacter>('rick-and-morty', id)
 </script>
 
 <template>
   <div class="py-8">
     <UContainer>
       <ApplicationBackToOverview to="/rick-and-morty" />
-      <ApplicationLoading v-if="status === 'pending'" />
-      <div v-else>
-        <pre>{{ data }}</pre>
-      </div>
+      <ApplicationDetail :status="status">
+        <ApplicationDetailHeader
+          :title="data.name"
+          :image="data.image"
+        />
+        <ApplicationDetailContent>
+          <ApplicationDetailContentItem
+            v-if="data.status"
+            label="Status"
+            :value="data.status"
+          />
+          <ApplicationDetailContentItem
+            v-if="data.gender"
+            label="Gender"
+            :value="data.gender"
+          />
+          <ApplicationDetailContentItem
+            v-if="data.species"
+            label="Species"
+            :value="data.species"
+          />
+          <ApplicationDetailContentItem
+            v-if="data.type"
+            label="Type"
+            :value="data.type"
+          />
+          <ApplicationDetailContentItem
+            v-if="data.origin"
+            label="Origin"
+            :value="data.origin.name"
+          />
+          <ApplicationDetailContentItem
+            v-if="data.location"
+            label="Location"
+            :value="data.location.name"
+          />
+        </ApplicationDetailContent>
+      </ApplicationDetail>
     </UContainer>
   </div>
 </template>
