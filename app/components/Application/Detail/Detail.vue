4ab8cd8e68b324547af51fbf8d3c5a9d6d9b07a7
steven.vos <steven.vos@iodigital.com>
2025-03-17 18:06:50 +0100

    feat(error status): add error component to handle error status

--- app/components/Application/Detail/Detail.vue
+++ app/components/Application/Detail/Detail.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 const props = defineProps<{
   status: string
+  to: string
 }>()
 </script>
 
 <template>
   <section class="my-8 px-3">
     <ApplicationLoading v-if="props.status === 'pending'" />
+    <ApplicationError
+      v-else-if="props.status === 'error'"
+      title="Oops.. Something went wrong"
+      message="Sorry, we couldn't fetch the data. Please try again later."
+      :to="props.to"
+    />
     <div v-else>
       <slot />
     </div>
---
+++ app/components/Application/Error.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+  message: string
+  to: string
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 items-center justify-center my-16 md:my-32">
+    <UIcon
+      name="mdi:alert-circle"
+      class="text-red-500 size-40"
+    />
+    <h1 class="text-red-500 text-2xl font-bold" v-text="props.title" />
+    <p class="text-red-500 text-lg font-medium" v-text="props.message" />
+    <UButton
+      variant="outline"
+      size="lg"
+      :to="props.to"
+      label="Go back"
+      color="error"
+      class="mt-4 ring-2"
+    />
+  </div>
+</template>
