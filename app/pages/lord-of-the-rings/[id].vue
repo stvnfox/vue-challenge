5614bece19490dcc4299cef66fcd6d77f1df6a24
steven.vos <steven.vos@iodigital.com>
2025-03-17 18:10:45 +0100

    feat(detail page): create detail page for lotr universe

--- app/pages/lord-of-the-rings/[id].vue
+++ app/pages/lord-of-the-rings/[id].vue
@@ -1,16 +1,44 @@
 <script setup lang="ts">
+import type { LordOfTheRingsCharacterCollectionItem } from '@nuxt/content'
+
 const { id } = useRoute('lord-of-the-rings-id').params
-const { data, status } = useUniverseDetail('lord-of-the-rings', id)
+const { data, status } = useUniverseDetail<LordOfTheRingsCharacterCollectionItem>('lord-of-the-rings', id)
+
+const name = computed(() => {
+  return [data.value.nameFirst, data.value.nameLast].filter(Boolean).join(' ')
+})
 </script>
 
 <template>
   <div class="py-8">
     <UContainer>
       <ApplicationBackToOverview to="/lord-of-the-rings" />
-      <ApplicationLoading v-if="status === 'pending'" />
-      <div v-else>
-        <pre>{{ data }}</pre>
-      </div>
+      <ApplicationDetail
+        :status="status"
+        to="/lord-of-the-rings"
+      >
+        <template v-if="data">
+          <ApplicationDetailHeader
+            :title="name"
+            :image="data.image"
+          />
+          <ApplicationDetailContent>
+            <ApplicationDetailContentItem
+              label="Age"
+              :value="data.age"
+            />
+            <ApplicationDetailContentItem
+              label="Height"
+              :value="data.height"
+            />
+            <ApplicationDetailContentListItem
+              label="Hobbies"
+              :value="data.hobbies"
+              type="string"
+            />
+          </ApplicationDetailContent>
+        </template>
+      </ApplicationDetail>
     </UContainer>
   </div>
 </template>
