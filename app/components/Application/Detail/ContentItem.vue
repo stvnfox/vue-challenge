e619d309d78d7404728bae21d5e074f781a5278f
steven.vos <steven.vos@iodigital.com>
2025-03-17 18:09:21 +0100

    feat(detail pages): add content-list-item building block component and changed content-item to handle numbers

--- app/components/Application/Detail/ContentItem.vue
+++ app/components/Application/Detail/ContentItem.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 const props = defineProps<{
   label: string
-  value: string
+  value: string | number
 }>()
 </script>
 
 <template>
   <div class="flex items-center">
-    <dt class="text-lg font-bold min-w-40" v-text="props.label" />
+    <dt class="text-lg font-bold min-w-60" v-text="props.label" />
     <dd class="text-blue-400 font-semibold" v-text="props.value" />
   </div>
 </template>
---
+++ app/components/Application/Detail/ContentListItem.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+interface ListItem { label: string, value: string | number }
+
+const props = defineProps<{
+  label: string
+  value: ListItem[] | string[]
+  type: 'string' | 'object'
+}>()
+</script>
+
+<template>
+  <div class="flex items-start">
+    <dt
+      class="text-lg font-bold min-w-60"
+      v-text="props.label"
+    />
+    <dd
+      v-if="props.type === 'string'"
+      class="text-blue-400 font-semibold capitalize"
+      v-text="props.value.join(', ')"
+    />
+    <dd
+      v-else
+      class="flex flex-col gap-2"
+    >
+      <div
+        v-for="item in (props.value as ListItem[])"
+        :key="item.label"
+        class="flex flex-col gap-2"
+      >
+        <span
+          class="font-bold capitalize text-neutral-500 dark:text-neutral-400"
+          v-text="item.label.replace('-', ' ')"
+        />
+        <span
+          class="text-blue-400 font-semibold"
+          v-text="item.value"
+        />
+      </div>
+    </dd>
+  </div>
+</template>
