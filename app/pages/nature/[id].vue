7c1b8d38432eae1b29d90ec994f62dd61757594a
steven.vos <steven.vos@iodigital.com>
2025-03-17 18:11:07 +0100

    feat(detail page): add detail page for nature universe

--- app/pages/nature/[id].vue
+++ app/pages/nature/[id].vue
@@ -1,16 +1,44 @@
 <script setup lang="ts">
+import type { NatureAnimalCollectionItem } from '@nuxt/content'
+
 const { id } = useRoute('nature-id').params
-const { data, status } = useUniverseDetail('nature', id)
+const { data, status } = useUniverseDetail<NatureAnimalCollectionItem>('nature', id)
 </script>
 
 <template>
   <div class="py-8">
     <UContainer>
       <ApplicationBackToOverview to="/nature" />
-      <ApplicationLoading v-if="status === 'pending'" />
-      <div v-else>
-        <pre>{{ data }}</pre>
-      </div>
+      <ApplicationDetail
+        :status="status"
+        to="/nature"
+      >
+        <template v-if="data">
+          <ApplicationDetailHeader
+            :title="data.name"
+            :icon="data.icon"
+          />
+          <ApplicationDetailContent>
+            <ApplicationDetailContentItem
+              label="Latin Name"
+              :value="data.nameLatin"
+            />
+            <ApplicationDetailContentItem
+              label="Size"
+              :value="data.size"
+            />
+            <ApplicationDetailContentItem
+              label="Lifespan"
+              :value="data.lifespan"
+            />
+            <ApplicationDetailContentListItem
+              label="Diet"
+              :value="data.diet"
+              type="string"
+            />
+          </ApplicationDetailContent>
+        </template>
+      </ApplicationDetail>
     </UContainer>
   </div>
 </template>
